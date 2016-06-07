d49c6fe0f4bad6c5cbf4c226a9cc037e08527c8a
motyros <motyros@gmail.com>
2016-06-07 15:41:19 +0300

    Add small express api with 'ProcessingController' in the 'post-processing-consumer'

--- post-processing-consumer/index.js
+++ post-processing-consumer/index.js
@@ -1,32 +1,28 @@
-var BusService = require('BusService'),
-	mongoose = require('mongoose'),
+var mongoose = require('mongoose'),
 	elasticsearch = require('elasticsearch'),
-	JobsService = require('JobsService'),
+	BusService = require('BusService'),
 	_ = require('lodash');
 
-// notify we're up, and check input
-console.log('Post processing consumer is up!');
-if (!isInputValid()) {
-	return "Bad input was received.";
-}
+var express = require('express'),
+	routes = require('./routes/index'),
+	app = express();
 
-// connect to our databases once so the service won't have to re-create connection each time
-connectDatabases();
+app.use('/', routes);
 
-// create bus
-// BusService = new BusService(process.env.REDIS_HOST, process.env.REDIS_PORT);
-// BusService.consume(process.env.QUEUE_NAME, handleMessage);
-handleMessage({
-	type: 'MetadataParser',
-	params: {
-		videoId: 'someVideoId',
-		relativePath: 'someVideoId.data',
-		method: {
-			standard: 'VisionStandard',
-			version: 1.0
-		}
+function main(arguments) {
+	// notify we're up, and check input
+	console.log('Post processing consumer is up!');
+	if (!isInputValid()) {
+		return "Bad input was received.";
 	}
-});
+
+	// connect to our databases once so the service won't have to re-create connection each time
+	connectDatabases();
+
+	// create bus
+	// BusService = new BusService(process.env.REDIS_HOST, process.env.REDIS_PORT);
+	// BusService.consume(process.env.QUEUE_NAME, handleMessage);
+}
 
 // Enforces basic validations on the environment input passed to process, such as mandatory parameters.
 // Later on, specific functions should enforce specific validations on their inputs
@@ -93,26 +89,4 @@ function connectElasticSearch() {
 	});
 }
 
-function handleMessage(message) {
-	// detect service job type
-	var jobType = process.env.JOB_TYPE;
-	// JOB_TYPE can be empty (handle all jobs), or a specific known JOB TYPE.
-	// message.type must be a known specific job.
-	if ((jobType && !JobsService.isKnownJobType(jobType)) ||
-		!JobsService.isKnownJobType(message.type)) {
-		console.log('Bad job type was inserted');
-		return;
-	}
-
-	// in case we are handling all job types, or,
-	// the job type is of our type
-	if (!jobType || message.type === jobType) {
-		var serviceName = JobsService.getServiceName(message.type);
-		service = require('./services/ProcessingServices/' + serviceName);
-		if (service)
-			service.start(message.params);
-		else
-			console.log('Bad service name');
-		return;
-	}
-}
+app.listen(4000, main);
