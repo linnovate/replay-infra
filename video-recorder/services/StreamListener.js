9eeab121b075b8ddd114422b1550d0f6fcf7afc6
dinzone <dinamitdana1@gmail.com>
2016-06-16 17:37:47 +0300

    1. Add infrastructure Test for the project, using mocha and chai. 2. Add unit test for the StreamListener.js + fix bugs that caused from the faild tests.

--- video-recorder/services/StreamListener.js
+++ video-recorder/services/StreamListener.js
@@ -11,7 +11,7 @@ var event = require('./EventEmitterSingleton'),
     dgram = require('dgram');
 
 // Defiend the Service name.
-const SERVICE_NAME = 'StreamListener',
+const SERVICE_NAME = '#StreamListener#',
     LOCALHOST = '0.0.0.0';
 
 // export our service.
@@ -32,54 +32,59 @@ function StreamListener() {
     /*                                                                                 */
     /***********************************************************************************/
     var startListen = function(params) {
-        const METHOD_NAME = 'StartListen';
-        console.log('#' + SERVICE_NAME + '#', METHOD_NAME, ' start running...\n params: ', JSON.stringify(params));
+        var promise = new Promise(function(resolve, reject) {
 
-        // check Port in params
-        if (!params.Port) {
-            event.emit('error','#' + SERVICE_NAME + '#' + 'Error on ' + METHOD_NAME + ' : There Is no Port To listen');
-            return;
-        } else {
-            _port = params.Port;
+            const METHOD_NAME = 'StartListen';
+            console.log(SERVICE_NAME, METHOD_NAME, ' start running...\n params: ', JSON.stringify(params));
 
-            // check Ip in params
-            _ip = params.Ip ? params.Ip : LOCALHOST
+            // check Port in params
+            if (!params || !params.Port) {
+                event.emit('unexceptedError', SERVICE_NAME + 'Error on ' + METHOD_NAME + ' : There Is no Port To listen');
+                return reject(SERVICE_NAME + 'Error on ' + METHOD_NAME + ' : There Is no Port To listen');
+            } else {
+                _port = params.Port;
 
-            // create socket server
-            _server = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+                // check Ip in params
+                _ip = (params.Ip && typeof(params.Ip) === 'string' && params.Ip.split('.').length == 4) ? params.Ip : LOCALHOST;
 
-            try {
-                // binding the server to listen to the address that given
-                _server.bind({ port: _port, address: _ip, exclusive: false }, () => {
-                    // check if the IP is not 0.0.0.0
-                    if (_ip != LOCALHOST) {
-                        _server.addMembership(_ip);
-                    }
-                    console.log('#' + SERVICE_NAME + '#','Binding To : ', _ip, ':', _port, ' succeed');
-                });
-            } catch (err) {
-                event.emit('error','#' + SERVICE_NAME + '#' +  'Error on ' + METHOD_NAME + ' : ' + err);
-                return;
+                // create socket server
+                _server = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+
+                try {
+                    // binding the server to listen to the address that given
+                    _server.bind({ port: _port, address: _ip, exclusive: false }, () => {
+                        // check if the IP is not 0.0.0.0
+                        if (_ip != LOCALHOST) {
+                            _server.addMembership(_ip);
+                        }
+                        console.log(SERVICE_NAME, 'Binding To : ', _ip, ':', _port, ' succeed');
+                        return resolve({ ip: _ip, port: _port });
+                    });
+                } catch (err) {
+                    console.log(SERVICE_NAME, ' ERROR:', err);
+                    event.emit('unexceptedError', SERVICE_NAME + 'Error on ' + METHOD_NAME + ' : ' + err);
+                    return reject(SERVICE_NAME + 'Error on ' + METHOD_NAME + ' : There Is no Port To listen');
+                }
             }
-        }
 
-        // on event of data flow
-        _server.on('message', (msg, rinfo) => {
-            console.log('#' + SERVICE_NAME + '#','Stop listening, Data Was detected at ', _ip, ':', _port, ' !');
-            // close the server so that the port will be open for the ffmpeg process to recording
-            _server.close();
-            // emmit an event so it could go next processing
-            event.emit('StreamingData');
+            // on event of data flow
+            _server.on('message', (msg, rinfo) => {
+                console.log(SERVICE_NAME, 'Stop listening, Data Was detected at ', _ip, ':', _port, ' !');
+                // close the server so that the port will be open for the ffmpeg process to recording
+                _server.close();
+                // emmit an event so it could go next processing
+                event.emit('StreamingData');
 
+            });
+
+            // when unexcepted error eccured.
+            _server.on('error', (err) => {
+                event.emit('unexceptedError', SERVICE_NAME + ' Unexcepted Error eccured while trying listen to the address ' + _ip + ':' + _port + ' : ' + err);
+                _server.close();
+            });
         });
 
-        // when unexcepted error eccured.
-        _server.on('error', (err) => {
-            event.emit('error','#' + SERVICE_NAME + '#' + ' Unexcepted Error eccured while trying listen to the address ' + _ip + ':' + _port + ' : ' + err);
-            _server.close();
-
-        });
-        return;
+        return promise;
     };
 
     return {
