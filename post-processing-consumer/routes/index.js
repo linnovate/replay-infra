a9d885797576ab1ce1339d90bb52051ac32b8cac
dinzone <dinamitdana1@gmail.com>
2016-06-26 11:36:11 +0300

    deleted post-processing

--- post-processing-consumer/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var ProcessingController = require('../controllers/ProcessingController')
-
-router.get('/start', ProcessingController.start);
-
-module.exports = router;
-
