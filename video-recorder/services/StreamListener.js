5f48e25d4cdfcb55b64f2a5728cf733aafa8df13
dinzone <dinamitdana1@gmail.com>
2016-06-27 12:27:41 +0300

    finished the streamlistener Tests + bug fix on streamlistener

--- video-recorder/services/StreamListener.js
+++ video-recorder/services/StreamListener.js
@@ -1,7 +1,7 @@
 /***********************************************************************************/
 /*                                                                                 */
 /*        Service for the stream listener.                                         */
-/*        listen to the port until some data will stream,                          */
+/*        listen to the Port until some data will stream,                          */
 /*        and then 'StreamingData' event is emiting.                               */
 /*                                                                                 */
 /***********************************************************************************/
@@ -14,7 +14,8 @@ var event = require('./EventEmitterSingleton');
 const SERVICE_NAME = '#StreamListener#',
 	LOCALHOST = '0.0.0.0',
 	MAX_BINDING_TRIES = 3,
-	TIME_TO_WAIT_AFTER_FAILED = 3000;
+	TIME_TO_WAIT_AFTER_FAILED = 3000,
+	IP_FORMAT = new RegExp(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/);
 
 // export our service.
 module.exports = StreamListener;
@@ -36,59 +37,64 @@ function StreamListener() {
 	/***********************************************************************************/
 	var startListen = function(params) {
 		var promise = new Promise(function(resolve, reject) {
+			var bindToTheAddress;
+			var finishedBind = false;
 			const METHOD_NAME = 'StartListen';
 			var bindingAttemptsCounts = 0;
 			console.log(SERVICE_NAME, METHOD_NAME, ' start running...\n params: ', JSON.stringify(params));
 
-			// check Port in params
-			if (!params || !params.Port) {
-				reject(SERVICE_NAME + ' Error on ' + METHOD_NAME + ' : There Is no Port To listen');
+			// check params
+			if (!params || !params.port || !params.ip) {
+				reject(SERVICE_NAME + ' Error on ' + METHOD_NAME + ' : Some of the parameters doesn\'t exist');
 			} else {
-				_port = params.Port;
+				_port = params.port;
 
 				// check Ip in params
-				_ip = (params.Ip && typeof params.Ip === 'string' && params.Ip.split('.').length === 4) ? params.Ip : LOCALHOST;
+				if (params.ip.toLowerCase() === 'localhost') {
+					_ip = LOCALHOST;
+				} else if (typeof params.ip === 'string' && params.ip.match(IP_FORMAT)) {
+					_ip = params.ip;
+				} else {
+					return reject(SERVICE_NAME + ' Error on ' + METHOD_NAME + ' : Some of the parameters doesn\'t vaildate');
+				}
+
+				console.log('finish validate the parameters');
 
 				// create socket server
 				_server = dgram.createSocket({ type: 'udp4', reuseAddr: true });
 
-				// function that called after the bind complete.
-				var afterBind = function() {
-					// check if the IP is not 0.0.0.0
-					if (_ip !== LOCALHOST) {
-						_server.addMembership(_ip);
-					}
-					console.log(SERVICE_NAME, 'Binding To : ', _ip, ':', _port, ' succeed');
-					return resolve({ ip: _ip, port: _port, numOfAttempts: bindingAttemptsCounts });
-				};
-
-				// bind to the address.
-				var bindToTheAddress = function() {
-					try {
-						_server.bind({ port: _port, address: _ip, exclusive: false }, afterBind);
-					} catch (err) {
-						console.log('try binding no', bindingAttemptsCounts, 'failed...\n', err);
-						throw new Error(err);
-					}
-				};
-
-				var tryBinding = function() {
-					var bindFailed = function() {
-						console.log('bindingAttemptsCounts', bindingAttemptsCounts);
+				// function that called after the binding.
+				var afterBind = function(err) {
+					if (err) {
 						bindingAttemptsCounts++;
+						console.log('try binding no', bindingAttemptsCounts, 'failed...\n', err);
 						if (bindingAttemptsCounts === MAX_BINDING_TRIES) {
+							_server.close();
 							return reject(SERVICE_NAME + ' Error on ' + METHOD_NAME + ' : Binding to source failed');
 						}
-						tryBinding();
-					};
-					try {
 						bindToTheAddress();
+					} else {
+						// check if the ip is not 0.0.0.0
+						if (_ip !== LOCALHOST) {
+							_server.addMembership(_ip);
+						}
+						console.log(SERVICE_NAME, 'Binding To : ', _ip, ':', _port, ' succeed');
+						finishedBind = true;
+						return resolve({ ip: _ip, port: _port, numOfAttempts: bindingAttemptsCounts });
+					}
+				};
+				// bind to the address.
+				bindToTheAddress = function() {
+					try {
+						_server.bind({ port: _port, address: _ip, exclusive: false });
 					} catch (err) {
-						setTimeout(bindFailed, TIME_TO_WAIT_AFTER_FAILED);
+						setTimeout(function() {
+							afterBind(err);
+						}, TIME_TO_WAIT_AFTER_FAILED);
 					}
 				};
 
-				tryBinding();
+				bindToTheAddress();
 
 				// on event of data flow
 				_server.on('message', (msg, rinfo) => {
@@ -99,11 +105,21 @@ function StreamListener() {
 					event.emit('StreamingData');
 				});
 
+				_server.on('listening', () => {
+					afterBind();
+				});
+
 				// when unexcepted error eccured.
 				_server.on('error', (err) => {
-					event.emit('unexceptedError_StreamListener', SERVICE_NAME + ' Unexcepted Error eccured while trying listen to the address ' +
-						_ip + ':' + _port + ' : ' + err);
-					_server.close();
+					if (finishedBind) {
+						event.emit('unexceptedError_StreamListener', SERVICE_NAME + ' Unexcepted Error eccured while trying listen to the address ' +
+							_ip + ':' + _port + ' : ' + err);
+						_server.close();
+					} else {
+						setTimeout(function() {
+							afterBind(err);
+						}, TIME_TO_WAIT_AFTER_FAILED);
+					}
 				});
 			}
 		});
