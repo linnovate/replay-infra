d8c44b4bc2f9745b1374bcc635d14b4c658ddf6e
oldwisenir <nirklinger471@gmail.com>
2016-07-12 10:24:48 +0300

    media url provider service

---
+++ media-url-provider/api/controllers/MediaController.js
@@ -0,0 +1,45 @@
+/**
+ * MediaController
+ *
+ * @description :: Server-side logic for managing media
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+var Promise = require('bluebird'),
+	Video = require('replay-schemas/Video');
+
+module.exports = {
+	'manifestUrl': function(req, res, next) {
+		var id = req.params.id;
+		getVideo(id)
+			.then(function(video) {
+				return WowzaService.getMpd(video.providerId);
+			})
+			.then(function(mpd) {
+				// res.setHeader('Content-Type', 'application/dash+xml');
+				res.json({ url: mpd });
+			})
+			.catch(function(err) {
+				if (err) {
+					console.log(err);
+					return res.badRequest(err);
+				}
+
+				res.serverError('There was an unexpected error retrieving mpd file.');
+			});
+	}
+};
+
+function getVideo(id) {
+	return Video
+		.findOne({ sourceId: id })
+		.then(function(video) {
+			if (!video) {
+				return Promise.reject('Video does not exist');
+			}
+			return Promise.resolve(video);
+		})
+		.catch(function(err) {
+			Promise.reject(err);
+		});
+}
