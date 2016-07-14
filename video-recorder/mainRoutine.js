8655888f68ea0c71e7ad162a5ee1608c5cc6e16e
oldwisenir <nirklinger471@gmail.com>
2016-07-14 16:53:30 +0300

    din is child

--- video-recorder/mainRoutine.js
+++ video-recorder/mainRoutine.js
@@ -151,13 +151,14 @@ function handleVideoSavingProcess(streamingSource) {
 		if (paths.telemetryPath) {
 			globals.metadataRelativeFilePath = paths.telemetryPath.substring(paths.videoPath.indexOf(STORAGE_PATH) + STORAGE_PATH.length);
 			globals.fileName = globals.metadataRelativeFilePath.split('/').pop().split('.')[0];
-			pathToWatch = globals.metadataRelativeFilePath;
+			pathToWatch = paths.telemetryPath;
 		}
 		if (paths.videoPath) {
 			globals.videoRelativeFilePath = paths.videoPath.substring(paths.videoPath.indexOf(STORAGE_PATH) + STORAGE_PATH.length);
 			globals.fileName = globals.videoRelativeFilePath.split('/').pop().split('.')[0];
-			pathToWatch = globals.videoRelativeFilePath;
+			pathToWatch = paths.videoPath;
 		}
+
 		startWatchFile(pathToWatch)
 			.then(function(timer) {
 				globals.fileWatcherTimer = timer;
