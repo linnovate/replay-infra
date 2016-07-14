a679ea92a476788a2b033d583bf4be65ebf22d6d
moshesharon <moshe@linnovate.net>
2016-07-14 12:59:45 +0300

    metadata to captions

--- consumer/services/processing-services/metadata-to-captions-service.js
+++ consumer/services/processing-services/metadata-to-captions-service.js
@@ -1,34 +1,36 @@
 /* global ElasticSearchService */
 var fs = require('fs');
-
+var start, end;
 module.exports.addSubtitle = function (index, type, videoId, sort, callback) {
 	console.log('MetadataToCaptions service started.');
 	ElasticSearchService.getDataByName(index, type, videoId, sort, function(res) {
-		var baseDate = null;
+		if (res === null) {
+			console.log('no Data Found for ', videoId);
+			return;
+		}
+		var baseDate = new Date(res[0]._source.timestamp);
 		res.forEach(function(r) {
 			if (baseDate === null) {
 				baseDate = new Date(r._source.timestamp);
 			}
 			var d = new Date(r._source.timestamp);
-
 			var timeDiff = Math.abs(d.getTime() - baseDate.getTime());
-
 			d = new Date(timeDiff);
-			var start = d.getUTCMinutes() + ':' + d.getUTCSeconds() +
+			start = d.getUTCMinutes() + ':' + d.getUTCSeconds() +
 			'.' + d.getUTCMilliseconds();
 			d.setSeconds(d.getSeconds() + 1);
-			var end = d.getUTCMinutes() + ':' + d.getUTCSeconds() +
+			end = d.getUTCMinutes() + ':' + d.getUTCSeconds() +
 			'.' + d.getUTCMilliseconds();
 			var timeLine = start + '-->' + end;
-			fs.writeFile(videoId + '.vtt', timeLine, function(err) {
+			fs.appendFile(videoId + '.vtt', timeLine + '\n', function(err) {
 				if (err) {
 					return console.log(err);
 				}
-				fs.writeFile(videoId + '.vtt', r, function(err) {
-					if (err) {
-						return console.log(err);
-					}
-				});
+			});
+			fs.appendFile(videoId + '.vtt', JSON.stringify(r) + '\n', function(err) {
+				if (err) {
+					return console.log(err);
+				}
 			});
 		});
 		console.log('The file was saved!');
