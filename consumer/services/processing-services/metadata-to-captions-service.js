e0fb9e1cb86693c36a052b5c614b0ef57ca41b7d
moshesharon <moshe@linnovate.net>
2016-07-17 12:02:36 +0300

    adding captions service to queue

--- consumer/services/processing-services/metadata-to-captions-service.js
+++ consumer/services/processing-services/metadata-to-captions-service.js
@@ -1,4 +1,3 @@
-/* global ElasticSearchService */
 var fs = require('fs');
 var start, end;
 
@@ -8,38 +7,39 @@ module.exports.start = function(params) {
 	console.log('=============================================');
 	console.log(JSON.stringify(params, null, 2));
 	console.log('=============================================');
-	addSubtitle(params.index, params.type, params.videoId, params.sort, params.callback);
+	addSubtitle(params);
 };
 
-function addSubtitle(index, type, videoId, sort, callback) {
-	ElasticSearchService.getDataByName(index, type, videoId, sort, function(res) {
-		if (res === null) {
-			console.log('no Data Found for ', videoId);
-			return;
+function addSubtitle(res) {
+	var path = process.env.STORAGE_PATH + '/captions/';
+	var videoId;
+	if (res === null) {
+		console.log('no Data Found ');
+		return;
+	}
+	videoId = res[0].videoId;
+	var baseDate = new Date(res[0]._source.timestamp);
+	res.forEach(function(r) {
+		if (baseDate === null) {
+			baseDate = new Date(r._source.timestamp);
 		}
-		var baseDate = new Date(res[0]._source.timestamp);
-		res.forEach(function(r) {
-			if (baseDate === null) {
-				baseDate = new Date(r._source.timestamp);
+		var d = new Date(r._source.timestamp);
+		var timeDiff = Math.abs(d.getTime() - baseDate.getTime());
+		d = new Date(timeDiff);
+		start = d.getUTCMinutes() + ':' + d.getUTCSeconds() + '.' + d.getUTCMilliseconds();
+		d.setSeconds(d.getSeconds() + 1);
+		end = d.getUTCMinutes() + ':' + d.getUTCSeconds() + '.' + d.getUTCMilliseconds();
+		var timeLine = start + '-->' + end;
+		fs.appendFile(path + videoId + '.vtt', timeLine + '\n', function(err) {
+			if (err) {
+				return console.log(err);
 			}
-			var d = new Date(r._source.timestamp);
-			var timeDiff = Math.abs(d.getTime() - baseDate.getTime());
-			d = new Date(timeDiff);
-			start = d.getUTCMinutes() + ':' + d.getUTCSeconds() + '.' + d.getUTCMilliseconds();
-			d.setSeconds(d.getSeconds() + 1);
-			end = d.getUTCMinutes() + ':' + d.getUTCSeconds() + '.' + d.getUTCMilliseconds();
-			var timeLine = start + '-->' + end;
-			fs.appendFile(videoId + '.vtt', timeLine + '\n', function(err) {
-				if (err) {
-					return console.log(err);
-				}
-			});
-			fs.appendFile(videoId + '.vtt', JSON.stringify(r) + '\n', function(err) {
-				if (err) {
-					return console.log(err);
-				}
-			});
 		});
-		console.log('The file was saved!');
+		fs.appendFile(path + videoId + '.vtt', JSON.stringify(r) + '\n', function(err) {
+			if (err) {
+				return console.log(err);
+			}
+		});
 	});
+	console.log('The file was saved!');
 }
