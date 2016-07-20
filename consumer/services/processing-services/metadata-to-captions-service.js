7f094f5dbb468e18946c98e375ba5ef5c9ae5d12
moshesharon <moshe@linnovate.net>
2016-07-20 14:17:01 +0300

    add variable time to captions

--- consumer/services/processing-services/metadata-to-captions-service.js
+++ consumer/services/processing-services/metadata-to-captions-service.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 var start, end;
+var dif, timeLine, timeDiff;
 
 module.exports.start = function(params) {
 	console.log('MetadataToCaptions service started.');
-
 	console.log('=============================================');
 	console.log(JSON.stringify(params, null, 2));
 	console.log('=============================================');
@@ -18,23 +18,26 @@ function addSubtitle(res) {
 		return;
 	}
 	videoId = res[0].videoId;
-	var baseDate = new Date(res[0].timestamp);
-	res.forEach(function(r) {
-		if (baseDate === null) {
-			baseDate = new Date(r.timestamp);
+
+	var baseDate = new Date(res[0]._source.timestamp);
+	end = getFormatedTime(new Date(0));
+	res.forEach(function(r, i) {
+		start = end;
+		if (i < (res.length - 1)) {
+			console.log(res[i + 1]._source.timestamp, r._source.timestamp);
+			dif = new Date(getTimeDiff(new Date(res[i + 1]._source.timestamp), baseDate));
+			end = getFormatedTime(dif);
+		} else {
+			dif.setSeconds(dif.getSeconds() + 1);
+			end = getFormatedTime(dif);
 		}
-		var d = new Date(r.timestamp);
-		var timeDiff = Math.abs(d.getTime() - baseDate.getTime());
-		d = new Date(timeDiff);
-		start = d.getUTCMinutes() + ':' + d.getUTCSeconds() + '.' + d.getUTCMilliseconds();
-		d.setSeconds(d.getSeconds() + 1);
-		end = d.getUTCMinutes() + ':' + d.getUTCSeconds() + '.' + d.getUTCMilliseconds();
-		var timeLine = start + '-->' + end;
+		timeLine = start + '-->' + end;
 		fs.appendFile(path + videoId + '.vtt', timeLine + '\n', function(err) {
 			if (err) {
 				return console.log(err);
 			}
 		});
+
 		fs.appendFile(path + videoId + '.vtt', JSON.stringify(r) + '\n', function(err) {
 			if (err) {
 				return console.log(err);
@@ -42,4 +45,18 @@ function addSubtitle(res) {
 		});
 	});
 	console.log('The file was saved!');
+
+	function getTimeDiff(time, baseDate) {
+		if (time === null || baseDate === null) {
+			console.log('error : ', 'time is missing');
+			return null;
+		}
+		timeDiff = Math.abs(time.getTime() - baseDate.getTime());
+		return timeDiff;
+	}
+
+	function getFormatedTime(time) {
+		return (time.getUTCMinutes() + ':' + time.getUTCSeconds() +
+			'.' + time.getUTCMilliseconds());
+	}
 }
