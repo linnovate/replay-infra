176b8481ad9e6dd495cd0d7c95b961d9a72d9819
Aviad Morshet <aviad10m@gmail.com>
2016-07-21 18:07:13 +0300

    Set maxUnackedMessages to be a process environment instead to be taken from config file

--- consumer/index.js
+++ consumer/index.js
@@ -11,12 +11,13 @@ var Promise = bluebird;
 
 // set mongoose promise library
 mongoose.Promise = bluebird.Promise;
+var _maxUnackedMessagesAmount = process.env.RABBITMQ_MAX_UNACKED_MESSAGES || 10;
 
 // notify we're up, and check input
 console.log('Consumer is up!');
 
 // extract command line params
-var jobType = process.argv[2];
+var _jobType = process.argv[2];
 if (!isInputValid()) {
 	console.log('Bad input was received.');
 	process.exit();
@@ -28,15 +29,17 @@ connectMongo(process.env.MONGO_HOST, process.env.MONGO_PORT, process.env.MONGO_D
 	.then(consumeRabbitMQ)
 	.catch(function(err) {
 		console.log(err);
+		process.exit();
 	});
 
 // enforces basic validations on the environment input passed to process,
 // such as mandatory parameters.
 // later on, specific functions should enforce specific validations on their inputs
 function isInputValid() {
-	console.log('Job type is: ', jobType);
+	console.log('Job type is: ', _jobType);
 	console.log('RabbitMQ host: ', process.env.RABBITMQ_HOST);
 	console.log('RabbitMQ max resend attempts: ', process.env.RABBITMQ_MAX_RESEND_ATTEMPTS);
+	console.log('RabbitMQ max unacked messages amount: ', process.env.RABBITMQ_MAX_UNACKED_MESSAGES);
 	console.log('Mongo host: ', process.env.MONGO_HOST);
 	console.log('Mongo port: ', process.env.MONGO_PORT);
 	console.log('Mongo database: ', process.env.MONGO_DATABASE);
@@ -45,7 +48,7 @@ function isInputValid() {
 	console.log('Files storage path: ', process.env.STORAGE_PATH);
 
 	// check mandatory parameter we can't continue without
-	if (!JobsService.isKnownJobType(jobType) || !process.env.MONGO_DATABASE || !process.env.STORAGE_PATH) {
+	if (!JobsService.isKnownJobType(_jobType) || !process.env.MONGO_DATABASE || !process.env.STORAGE_PATH) {
 		return false;
 	}
 
@@ -59,19 +62,18 @@ function connectRabbitMQ() {
 
 function consumeRabbitMQ() {
 	// get the matching queue name of the job type
-	var queueName = JobsService.getQueueName(jobType);
-	var maxMessagesAmount = JobsService.getQueueMaxMessagesAmount(jobType);
-	return rabbit.consume(queueName, maxMessagesAmount, handleMessage);
+	var queueName = JobsService.getQueueName(_jobType);
+	return rabbit.consume(queueName, _maxUnackedMessagesAmount, handleMessage);
 }
 
-function handleMessage(message, err, done) {
+function handleMessage(message, error, done) {
 	console.log('Lifting appropriate service...');
 
 	// get the appropriate service name and start it
-	var serviceName = JobsService.getServiceName(jobType);
+	var serviceName = JobsService.getServiceName(_jobType);
 	var service = require(path.join(__dirname, 'services/processing-services/', serviceName));
 	if (service) {
-		service.start(message, err, done);
+		service.start(message, error, done);
 	} else {
 		console.log('Bad service name');
 	}
