10a0c6c4ba704710abf85a674620ccfa62bbbdde
Aviad Morshet <aviad10m@gmail.com>
2016-07-26 12:05:49 +0300

    Completed tests for consumer

--- consumer/test/consumer.test.js
+++ consumer/test/consumer.test.js
@@ -1,8 +1,47 @@
-require('./config');
+var path = require('path');
+var fork = require('child_process').fork;
+var consumerPath = path.join(__dirname, '../index');
 
 describe('consumer tests', function() {
+	describe('bad input tests', function() {
+		it('lacks job type', function(done) {
+			var consumer = fork(consumerPath, undefined);
+			consumer.on('close', function(exitCode) {
+				expect(exitCode).to.equal(1);
+				done();
+			});
+		});
 
-	it('consumer tests', function() {
-		return true;
+		it('bad job type', function(done) {
+			var consumer = fork(consumerPath, ['badJobType']);
+			consumer.on('close', function(exitCode) {
+				expect(exitCode).to.equal(1);
+				done();
+			});
+		});
+
+		it('lacks mongo database', function(done) {
+			var consumer = fork(consumerPath, ['SaveVideo'], {
+				env: {
+					MONGO_DATABASE: undefined
+				}
+			});
+			consumer.on('close', function(exitCode) {
+				expect(exitCode).to.equal(1);
+				done();
+			});
+		});
+
+		it('lacks storage path', function(done) {
+			var consumer = fork(consumerPath, ['SaveVideo'], {
+				env: {
+					STORAGE_PATH: undefined
+				}
+			});
+			consumer.on('close', function(exitCode) {
+				expect(exitCode).to.equal(1);
+				done();
+			});
+		});
 	});
 });
