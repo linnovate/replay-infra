e981598426ebcc7622d868d7fa82363bfae0f376
dinzone <dinamitdana1@gmail.com>
2016-08-11 12:20:31 +0300

    move the node module from the service to the consumer directory

--- consumer/processing-services/video-processing-service/index.js
+++ consumer/processing-services/video-processing-service/index.js
@@ -1,4 +1,27 @@
-var ffmpeg = require('replay-ffmpeg');
 
-module.exports.start = function (params, error, done) {
+const CONSUMER_NAME = '#video-proccesing#';
+
+module.exports.start = function(params, error, done) {
+	if (!paramsIsValid(params)) {
+		console.log(CONSUMER_NAME, 'params are not valid');
+		return error();
+	}
+
+	proccesVideo()
+		.then(function() {
+
+		})
+		.catch(function() {
+
+		});
 };
+
+// validate the params.
+function paramsIsValid(params) {
+	return true;
+}
+
+// understand what ts file we deal (video/data/video and data) and manipulate it.
+function proccesVideo() {
+	return true;
+}
