0bb429234d8354e434cdefb18043238fb6d050a1
motyros <motyros@gmail.com>
2016-08-15 17:53:44 +0300

    fix 'metadata-to-captions-service' - Currently copy captions file to a HARDCODED destination path!

--- consumer/processing-services/captions-service/captions-to-destination-service.js
+++ consumer/processing-services/captions-service/captions-to-destination-service.js
@@ -10,97 +10,93 @@ var _jobStatusTag = 'captions-copied-to-destination';
 module.exports.start = function(params, error, done) {
 	console.log('CaptionsToDestination started.');
 
-	if (!validateInput(params)) {
-		console.log('CaptionsToDestination - Some vital parameters are missing.');
-		return error();
-	}
-	_transactionId = params.transactionId;
-	// Make sure we haven't done this job already
-	JobsService.findJobStatus(_transactionId)
+	validateInput(params)
+		.then(function(transactionId) {
+			_transactionId = params.transactionId;
+			// Make sure we haven't done this job already
+			return JobsService.findJobStatus(_transactionId);
+		})
 		.then(function(jobStatus) {
 			if (jobStatus.statuses.indexOf(_jobStatusTag) > -1) {
 				// case we've already performed the action, ack the message
-				done();
-			} else {
-				copyToDestination(params, error, done);
+				return Promise.resolve();
 			}
-			// always resolve as copyToDestination is async
-			return Promise.resolve();
+			return copyToDestination(params, error, done);
 		})
 		.catch(function(err) {
-			if (err) {
-				console.log(err);
-				// NOTE: If we've failed here we probably haven't copied to destination; hence, call error().
-				error();
-			}
+			console.log(err);
+			error();
 		});
 };
 
 function validateInput(params) {
-	console.log('Transaction id: ', params.transactionId);
-	console.log('Captions file name: ', params.captionsFileName);
-	console.log('Captions relative path: ', params.captionsRelativePath);
-	console.log('Captions destination path: ', process.env.CAPTIONS_DESTINATION_PATH);
+	console.log('Transaction id:', params.transactionId);
+	console.log('Captions videoId:', params.videoId);
+	console.log('Captions file name:', params.captionsFileName);
+	console.log('Captions relative path:', params.captionsRelativePath);
+	console.log('Captions storage path:', process.env.STORAGE_PATH);
 
 	// validate params
-	if (!params.transactionId || !params.captionsFileName || !params.captionsRelativePath ||
-		!process.env.STORAGE_PATH || !process.env.CAPTIONS_DESTINATION_PATH) {
-		return false;
+	if (!params.transactionId || !params.videoId || !params.captionsFileName || !params.captionsRelativePath || !process.env.STORAGE_PATH) {
+		return Promise.reject('CaptionsToDestination - Some vital parameters are missing.');
 	}
 
-	return true;
+	return Promise.resolve(params.transactionId);
 }
 
 function copyToDestination(params, error, done) {
 	var sourceFilePath = path.join(process.env.STORAGE_PATH, params.captionsRelativePath, params.captionsFileName);
-	var destinationFilePath = path.join(process.env.DROP_FOLDER_PATH, params.captionsFileName);
+	var destinationFilePath = findCaptionsDestinationPath(params);
 
-	console.log(sourceFilePath);
-	console.log(destinationFilePath);
+	console.log('sourceFilePath:', sourceFilePath);
+	console.log('destinationFilePath', destinationFilePath);
 
 	// copy captions file into destination
-	copyFile(sourceFilePath, destinationFilePath, function(err) {
-		if (err) {
-			console.log('Error copying captions to destination: ', err);
-			error();
-		}
-		console.log('Captions successfuly copied to destination.');
-		// update status
-		return updateJobStatus()
-			.then(done)
-			.catch(function(err) {
-				if (err) {
-					console.log(err);
-					// NOTE: We do not error() on this, else duplicate entries would
-					// be uploaded to Kaltura!
-					done();
-				}
-			});
-	});
+	return copyFile(sourceFilePath, destinationFilePath)
+		.catch(function(err) {
+			if (err) {
+				console.log('Error copying captions to destination:', err);
+				error(); // NOTE: If we've failed here we probably haven't copied nothing to destination and that is why we call error().
+			}
+		})
+		.then(function() {
+			console.log('Captions successfuly copied to destination.');
+			return updateJobStatus()
+				.catch(function(err) {
+					if (err) {
+						console.log('CaptionsToDestination - Error on updateJobStatus:', err);
+						// NOTE: We do not call error() on this, else duplicate captions would be copied to destination
+						done();
+						return Promise.resolve();
+					}
+				});
+		});
 }
 
-function copyFile(source, target, cb) {
-	var cbCalled = false;
+function findCaptionsDestinationPath(params) {
+	// TODO: Find destination path by the given params
+	// var destinationPath = '/mnt/kaltura_content/captions/acona_matta.vtt';
+	var destinationPath = '/mnt/kaltura_content/20160728/0/0_ervwr6f7_0_83qjoyd1_2.vtt';
 
-	var rd = fs.createReadStream(source);
-	rd.on('error', function(err) {
-		doneCopy(err);
-	});
-	var wr = fs.createWriteStream(target);
-	wr.on('error', function(err) {
-		doneCopy(err);
-	});
-	wr.on('close', function(ex) {
-		doneCopy();
-	});
-	rd.pipe(wr);
+	console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+	console.log('Currently destination path HARDCODED!');
+	console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+	return destinationPath;
+}
 
-	function doneCopy(err) {
-		if (!cbCalled) {
-			cbCalled = true;
-			cb(err);
-		}
-	}
+function copyFile(source, target) {
+	return new Promise(function(resolve, reject) {
+		var rd = fs.createReadStream(source);
+		rd.on('error', function(err) {
+			reject('ReadStream error on copy captions: \n' + err);
+		});
+		var wr = fs.createWriteStream(target);
+		wr.on('error', function(err) {
+			reject('WriteStream error on copy captions: \n' + err);
+		});
+		wr.on('finish', resolve);
+		rd.pipe(wr);
+	});
 }
 
 function updateJobStatus() {
--- consumer/processing-services/captions-service/metadata-to-captions-service.js
+++ consumer/processing-services/captions-service/metadata-to-captions-service.js
@@ -3,15 +3,17 @@ var fs = require('fs'),
 
 var Promise = require('bluebird'),
 	mkdirp = require('mkdirp'),
+	rabbit = require('replay-rabbitmq'),
 	JobsService = require('replay-jobs-service');
 
 var _transactionId;
 var _jobStatusTag = 'created-captions-from-metadata';
 
 var LAST_CAPTIONS_TIME = 1; // in seconds
+var CAPTIONS_PATH = 'captions';
 
 module.exports.start = function(params, error, done) {
-	console.log('MetadataToCaptions service startTimeed.');
+	console.log('MetadataToCaptions service started.');
 
 	if (!validateInput(params)) {
 		console.log('MetadataToCaptions - Some vital parameters are missing.');
@@ -26,7 +28,7 @@ module.exports.start = function(params, error, done) {
 				// case we've already performed the action, ack the message
 				return Promise.resolve();
 			}
-			return tryCreateCaptions(params.metadatas);
+			return performCaptionsChain(params.metadatas);
 		})
 		.then(function() {
 			done();
@@ -42,7 +44,7 @@ module.exports.start = function(params, error, done) {
 };
 
 function validateInput(params) {
-	console.log('Transaction id: ', params.transactionId);
+	console.log('Transaction id:', params.transactionId);
 
 	// validate params
 	if (!params.transactionId) {
@@ -51,22 +53,28 @@ function validateInput(params) {
 	return true;
 }
 
-function tryCreateCaptions(metadatas) {
+function performCaptionsChain(metadatas) {
 	if (metadatas && metadatas.length > 0) {
-		return createCaptions(metadatas);
+		return createCaptions(metadatas)
+			.then(produceCaptionsToDestinationJob)
+			.catch(function(err) {
+				return Promise.reject(err);
+			});
 	}
+	// else:
 	console.log('MetadataToCaptions - No metadatas receieved.');
 	return Promise.resolve();
 }
 
 function createCaptions(metadatas) {
-	var startTime, endTime, currentTimestamp;
-	var timeDiff, timeLine, fileName;
+	var startTime, endTime, currentTimestamp, timeDiff, timeLine;
 
-	var captionsPath = path.join(process.env.STORAGE_PATH, 'captions');
+	var captionsPath = path.join(process.env.STORAGE_PATH, CAPTIONS_PATH);
 	checkAndCreatePath(captionsPath);
 
 	var videoId = metadatas[0].videoId;
+	var fileName = videoId + '.vtt';
+	var captionsFullPath = path.join(captionsPath, fileName);
 	var baseTimestamp = new Date(metadatas[0].timestamp);
 	endTime = getFormatedTime(new Date(0));
 
@@ -82,8 +90,7 @@ function createCaptions(metadatas) {
 				endTime = getFormatedTime(timeDiff);
 			}
 			timeLine = startTime + '-->' + endTime;
-			fileName = path.join(captionsPath, videoId + '.vtt');
-			return fs.appendFile(fileName, timeLine + '\n' + JSON.stringify(item) + '\n\n', function(err) {
+			return fs.appendFile(captionsFullPath, timeLine + '\n' + JSON.stringify(item) + '\n\n', function(err) {
 				if (err) {
 					return Promise.reject(err);
 				}
@@ -91,7 +98,7 @@ function createCaptions(metadatas) {
 			});
 		}).then(function() {
 			console.log('Captions file created successfully!');
-			return Promise.resolve();
+			return Promise.resolve({ videoId: videoId, captionsRelativePath: CAPTIONS_PATH, captionsFileName: fileName });
 		})
 		.catch(function(err) {
 			return Promise.reject(err);
@@ -117,6 +124,23 @@ function getTimeDiff(currentTimestamp, baseTimestamp) {
 function getFormatedTime(time) {
 	return (time.getUTCMinutes() + ':' + time.getUTCSeconds() +
 		'.' + time.getUTCMilliseconds());
+}
+
+function produceCaptionsToDestinationJob(params) {
+	console.log('Producing CaptionsToDestination job...');
+
+	var message = {
+		videoId: params.videoId,
+		captionsRelativePath: params.captionsRelativePath,
+		captionsFileName: params.captionsFileName,
+		transactionId: _transactionId
+	};
+
+	var queueName = JobsService.getQueueName('CaptionsToDestination');
+	if (queueName) {
+		return rabbit.produce(queueName, message);
+	}
+	return Promise.reject(new Error('Could not find queue name of the inserted job type'));
 }
 
 function updateJobStatus() {
