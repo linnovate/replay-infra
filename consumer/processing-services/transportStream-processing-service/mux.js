d8bd268f79a6a47dd21ce2dac7e92cab1bf965e0
dinzone <dinamitdana1@gmail.com>
2016-08-28 15:10:34 +0300

    change the paths that send to the ffmpeg function and add option to take the file from another absolute path

--- consumer/processing-services/transportStream-processing-service/mux.js
+++ consumer/processing-services/transportStream-processing-service/mux.js
@@ -1,18 +1,27 @@
 var ffmpeg = require('replay-ffmpeg');
 var Promise = require('bluebird');
+var path = require('path');
 
 // export the process job for the video-standard version 0.9 (video/data).
 module.exports = processTS;
 
+var STORAGE_PATH = process.env.STORAGE_PATH;
+
 function processTS(params) {
 	if (!paramsIsValid(params)) {
-		return Promise.reject('video-standard work with video/data, and there was none');
+		return Promise.reject(new Error('video-standard work with video/data, and there was none'));
 	}
-	var videoPath = params.videoRelativePath,
-		dataPath = params.dataRelativePath;
+	var tsFilePath = params.fileRelativePath;
+	// set the paths to the new files that will create.
+	var relativePathTo = path.parse(tsFilePath);
+	relativePathTo = path.join(STORAGE_PATH, relativePathTo.dir, relativePathTo.name);
+	// set the storage path from the params or default.
+	var storagePath = params.filesStoragePath || STORAGE_PATH;
+	// set the path to the file.
+	tsFilePath = path.join(storagePath, tsFilePath);
 	return new Promise(function(resolve, reject) {
 		// converting and extracting the data, wait until finishing the work and return promise.
-		ffmpeg.convertAndExtract({ videoPath: videoPath, dataPath: dataPath });
+		ffmpeg.convertAndExtract({ inputPath: tsFilePath, outputPath: relativePathTo });
 		ffmpeg.on('finishConvertAndExtract', resolve);
 		ffmpeg.on('errorOnConvertAndExtract', reject);
 	});
