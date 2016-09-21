6a340025a25e505d9a2ce8078b48f6484ac57dad
Aviad Morshet <aviad10m@gmail.com>
2016-09-21 14:26:50 +0300

    Began integration tests

--- consumer/test/integration/integration.test.js
+++ consumer/test/integration/integration.test.js
@@ -1,24 +1,56 @@
-// var config = require('../config');
+var spawn = require('child-process-promise').spawn,
+	Promise = require('bluebird');
 
-// var SaveVideoService = require('../../processing-services/save-video-service');
-// var Video = require('replay-schemas/Video'),
-// 	rabbit = require('replay-rabbitmq'),
-// 	JobsService = require('replay-jobs-service');
+var config = require('../config');
+var JobsService = require('replay-jobs-service');
 
-// // Test that the service produces a message with the expected values
+var _childProcesses = [];
 
-// describe('integration tests', function () {
-// 	before(function () {
-// 		config.resetEnvironment();
-// 		return config.connectServices()
-// 			.then(config.wipeMongoCollections);
-// 	});
+describe('integration tests', function () {
+	before(function () {
+		config.resetEnvironment();
+		return config.wipeMongoCollections()
+			.then(config.deleteAllQueues)
+			.then(() => liftConsumers());
+	});
 
-// 	after(function () {
-// 		return config.wipeMongoCollections()
-// 			.then(config.deleteAllQueues);
-// 	});
+	after(function () {
+		return config.wipeMongoCollections()
+			.then(config.deleteAllQueues)
+			.then(() => closeConsumers());
+	});
 
-// 	describe('save-video-service', function () {
-// 	});
-// });
+	describe('save-video-service', function () {
+	});
+});
+
+function liftConsumers() {
+	var consumersPromises = [];
+	var jobsConfig = JobsService.getAllJobsConfig();
+	jobsConfig.forEach(function (jobConfig) {
+		var jobName = jobConfig.type;
+
+		var promise = spawn('node', ['../index.js', jobName]);
+		var childProcess = promise.childProcess;
+		// track the child process
+		_childProcesses.push(childProcess);
+
+		// redirect outputs
+		childProcess.stdout.on('data', function (data) {
+			console.log('%s job stdout: ', jobName, data.toString());
+		});
+		childProcess.stderr.on('data', function (data) {
+			console.log('%s job stderr: ', jobName, data.toString());
+		});
+
+		consumersPromises.push(promise);
+	});
+
+	return Promise.all(consumersPromises);
+}
+
+function closeConsumers() {
+	_childProcesses.forEach(function(proc) {
+		proc.kill('SIGINT');
+	});
+}
